395cd8a8a9043d355b414c8a11c530abfb1f68f2
edwinfreze <edvin.freze@gmail.com>
2020-06-19 18:07:17 +0300

    1st day java exercises

--- SEF/src/sef/module3/activity/OperatorActivity.java
+++ SEF/src/sef/module3/activity/OperatorActivity.java
@@ -12,8 +12,8 @@ public class OperatorActivity {
 	public static void main(String[] args) {
 		
 		// Declare numbers to be operated
-		int i = 8;
-		int j = 5;
+		int i = 11;
+		int j = 6;
 		
 		// Subtract numbers
 		int result = i-j;
@@ -22,9 +22,10 @@ public class OperatorActivity {
 		System.out.println("Difference = " + result);
 		
 		// Add numbers
-		
+		int resultAdding = i+j;
 		
 		// Print result
+		System.out.println("Adding result = " + resultAdding);
 		
 		
 	}
--- SEF/src/sef/module3/activity/VariableAssignmentActivity.java
+++ SEF/src/sef/module3/activity/VariableAssignmentActivity.java
@@ -14,6 +14,9 @@ public class VariableAssignmentActivity {
 	 */
 	public static void main(String[] args) {
 		
+		int a = 5;
+		a = 6;
+		System.out.println(a);
 		// 1- Declare a variable of type int and assign it default value.
 		// 2- Update the value 
 		// 3- Print updated value to the console
